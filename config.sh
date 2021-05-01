ba207ab9b360de4d4ce1f691f675addc1c56179c
Maik Kazmierski <maik_86@web.de>
2021-05-01 20:55:05 +0200

    readme

--- config.sh
+++ config.sh
@@ -3,14 +3,14 @@
 read -p "EMAIL: " email
 read -p "DOMAIN: " domain
 read -p "DASHBOARD_AUTH_USERNAME: " username
-read -sp "DASHBOARD_AUTH_PASSWORDHASH: " hash
-read -sp "DASHBOARD_AUTH_PASSWORDSALT: " salt
+read -p "DASHBOARD_AUTH_PASSWORDHASH:" hash
+read -p "DASHBOARD_AUTH_PASSWORDSALT:" salt
 
 echo "EMAIL=$email
 #DOMAIN=$domain
 #DASHBOARD_AUTH_USERNAME=$username
-#DASHBOARD_AUTH_PASSWORDHASH=hash
-#DASHBOARD_AUTH_PASSWORDSALT=salt" > hornet
+#DASHBOARD_AUTH_PASSWORDHASH=$hash
+#DASHBOARD_AUTH_PASSWORDSALT=$salt" > /etc/default/hornet
 
 cp hornet.service /etc/systemd/system/hornet.service && rm hornet.service
 systemctl enable hornet.service
