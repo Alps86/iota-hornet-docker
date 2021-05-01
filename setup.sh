0aa63aba7e916fbc5b380cbdb2df3ac06004e6d8
Maik Kazmierski <maik_86@web.de>
2021-05-01 18:28:33 +0200

    setup

--- setup.sh
+++ setup.sh
@@ -16,6 +16,7 @@ add-apt-repository \
    stable"
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
+/etc/init.d/docker start || true
 curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
